b35c6b2c279c6b70518563c6ff21c4257e9f556e
Jose Soledispa <129227154+JoseSoledispaV@users.noreply.github.com>
2024-05-07 17:48:45 -0500

    Practica 2
    SE DEFINIERON LAS 3 ENTIDADES
    Y SE AGREGARON ATRIBUTOS

---
+++ Practica 1/app.js
@@ -0,0 +1,4 @@
+const message:string=`Prueba de ejemplo 1`;
+const alumno={ nombre: 'Juan', edad: 20, curso: 'Angular'}
+
+export {message, alumno}
---
+++ Practica 1/migration.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "PC" (
+    "id" SERIAL NOT NULL,
+    "descripcion" TEXT NOT NULL,
+    "serie" TEXT,
+
+    CONSTRAINT "Pc_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Software" (
+    "id" SERIAL NOT NULL,
+    "idpc" TEXT,
+
+    CONSTRAINT "Software_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Bitacora" (
+    "id" SERIAL NOT NULL,
+    "pcId" INTEGER NOT NULL,
+    "softwareId" INTEGER NOT NULL,
+    "fecha" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Bitacora_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PC_descripcion_key" ON "PC"("idpc");
+
+-- AddForeignKey
+ALTER TABLE "Software" ADD CONSTRAINT "Software_pcId_fkey" FOREIGN KEY ("ciudadanoaId") REFERENCES "Ciudadanoa"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Software" ADD CONSTRAINT "Software_softwareId_fkey" FOREIGN KEY ("preguntaaId") REFERENCES "Preguntaa"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
