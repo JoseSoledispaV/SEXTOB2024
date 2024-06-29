dc1e5e0446755d5e902e6864dfcd9bc424e1dc05
Jose Soledispa <129227154+JoseSoledispaV@users.noreply.github.com>
2024-06-29 00:16:46 -0500

    registro y configuracion de usuario

--- PARCIAL 2/PROYECTO APW2 - PARCIAL 2/backend/src/app.module.ts
+++ PARCIAL 2/PROYECTO APW2 - PARCIAL 2/backend/src/app.module.ts
@@ -13,8 +13,20 @@ import { CategoryModule } from './admin/category/category.module';
 
 import { MarketModule } from './client/market/market.module';
 import { CompanyCategoryModule } from './admin/company-category/company-category.module';
+import { HoursOperationModule } from './admin/hours-peration/hours-peration.module';
 import { PetitionModule } from './deliveryman/petition/petition.module';
+import { ChatModule } from './chat/chat.module';
+import { LocationWsModule } from './location-ws/location-ws.module';
+import { AddressModule } from './client/address/address.module';
+import { NotificationModule } from './notification/notification.module';
+import { BalanceModule } from './client/balance/balance.module';
+import { EmailModule } from './email/email.module';
+import { EnrollmentModule } from './manager/enrollment/enrollment.module';
 import { StoreManagerModule } from './manager/store-manager/store.manager.module';
+import { PaymentsModule } from './client/payments/payments.module';
+import { CreditModule } from './admin/credit/credit.module';
+import { RequestModule } from './manager/request/request.module';
+import { GroupModule } from './admin/group/group.module';
 import { CompanyRiderModule } from './admin/company-rider/company-rider.module';
 
 
@@ -40,8 +52,20 @@ import { CompanyRiderModule } from './admin/company-rider/company-rider.module';
     MarketModule,
     CategoryModule,
     CompanyCategoryModule,
+    HoursOperationModule,
     PetitionModule,
+    ChatModule,
+    LocationWsModule,
+    AddressModule,
+    NotificationModule,
+    BalanceModule,
+    EmailModule,
+    EnrollmentModule,
     StoreManagerModule,
+    PaymentsModule,
+    CreditModule,
+    RequestModule,
+    GroupModule,
     CompanyRiderModule,
   ],
   controllers: [],
